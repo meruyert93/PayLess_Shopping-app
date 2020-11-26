57d00c7743cddfacaf7833148e13bba8d489523d
Meruyert Karim <meruyert.karim@nu.edu.kz>
2020-11-26 23:46:50 +0100

    Total Price Updated

--- js/index.js
+++ js/index.js
@@ -155,10 +155,14 @@ for (let i = 0; i < removerCartItemButtons.length; i++){
 function updateCartTotal() {
     let cartItemContainer = document.getElementsByClassName('cart-items')[0]
     let cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    let total = 0;
     for (let i = 0; i<cartRows.length; i++) {
         let cartRow = cartRows[i]
         let priceElement = cartRow.getElementsByClassName('cart-price')[0]
-        let quantityElement = cartRow.getElementsByClassName('cart-quantity-input')
-        console.log(priceElement, quantityElement);
+        let quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        let price = parseFloat(priceElement.innerText.replace('$', ''));
+        let quantity = quantityElement.value
+        total = total + (price *quantity)
     }
+    document.getElementsByClassName('cart-total-price')[0].innerText = total;
 }
